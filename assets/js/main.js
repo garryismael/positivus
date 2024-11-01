22ad18e52be63325cff36bb2a87d10450d43caa4
Garry <tahinjanaharygarry@gmail.com>
2024-11-01 21:58:54 +0300

    add accordions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
 const emblaNode = document.querySelector(".embla");
+const container = document.getElementsByClassName("container");
+const acc = document.getElementsByClassName("accordion");
+const panels = document.getElementsByClassName("panel");
+const paragraphs = document.querySelectorAll(".panel > p");
+
 const options = {
   loop: false,
   active: true,
@@ -7,3 +12,29 @@ const options = {
   },
 };
 const emblaApi = EmblaCarousel(emblaNode, options);
+
+// Accordions
+for (let i = 0; i < container.length; i++) {
+  container[i].addEventListener("click", function () {
+    for (let j = 0; j < panels.length; j++) {
+      if (j !== i) {
+        acc[j].classList.remove("active");
+        container[j].classList.remove("active");
+        panels[j].style.maxHeight = null;
+        paragraphs[j].style.maxHeight = null;
+      }
+    }
+
+    acc[i].classList.toggle("active");
+    container[i].classList.toggle("active");
+    let panel = panels[i];
+    let paragraph = paragraphs[i];
+    if (panel.style.maxHeight) {
+      paragraph.style.maxHeight = null;
+      panel.style.maxHeight = null;
+    } else {
+      paragraph.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
